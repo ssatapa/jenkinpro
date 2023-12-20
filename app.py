68692eef28a1428d071bdc8f892ef2402354d5a9
Subhalaxmi Satapathy <subhasatapathy91@gmail.com>
2023-12-20 22:52:54 +0530

    added new code

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def create_app():
 
     @app.route('/')
     def home():
-        return quote('Hello, World! This is a simple Flask app by subhalaxmi.')
+        return quote('Hello, Subhalaxmi')
 
     return app
 
